caeca64053398f72bcae992f63d01ff2bbb5b146
VishnuBharath-Reddy <vishnubharathreddyaekasi@gmail.com>
2026-05-24 15:40:34 -0500

    Started My AI Engineering

---
+++ python-practice/day1.py
@@ -0,0 +1,14 @@
+print("I am an AI Engineer...")
+
+role = input("what is your role: ")
+print("My role is" , role)  
+
+print("lets build a simple calculator")
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number:  "))
+
+print("Addition = ", a + b)
+print("Substraction = ", a - b)
+print("Mutliplication = ", a * b)
+print("Division = ", a / b)
